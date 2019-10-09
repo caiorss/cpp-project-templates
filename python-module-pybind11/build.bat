c58049c4c0c4b1542d25b8dede95ecc66694fa94
CaioRS <caiorss.rodrigues@gmail.com>
2019-10-09 16:43:50 -0300

    Fix python-module-pybind11/build.bat window build helper script.

--- python-module-pybind11/build.bat
+++ python-module-pybind11/build.bat
@@ -2,5 +2,5 @@
 
 REM Automate CMake Compilation on Windows     
 
-cmake -H. -Bbuild -G "Visual Studio 15 2017 Win64"
+cmake -H. -Bbuild -G "Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Debug
 cmake --build build --target    
